1b684efd26c726b99a7eb0483eabce502fd8b412
BespalovSergey <50576059+BespalovSergey@users.noreply.github.com>
2019-07-14 22:03:35 +0300

    Update vk_dialog.py

--- vk_dialog.py
+++ vk_dialog.py
@@ -7,9 +7,10 @@ from vk_api.keyboard import VkKeyboard ,VkKeyboardColor
 
 
 class MyVkBot():
+  
   def __init__(self, questions):
     self.questions = questions
-    self.keys = list(questions.keys())
+   
     self.r = redis.Redis(host= os.environ['redis_adress'],
               password = os.environ['redis_password'],
               port = os.environ['redis_port'],
@@ -27,32 +28,35 @@ class MyVkBot():
     
   def get_question_index(self , chat_id):
     index = -1
-    redis_data = self.r.get(chat_id)
+    redis_key = 'vk_{}'.format(chat_id)
+    redis_data = self.r.get(redis_key)
     if redis_data:
-      index = self.keys.index(redis_data)
-    return index   
+      index = redis_data
+    return int(index)   
 
 
   def new_question(self , user_id ,index):  
+    redis_key = 'vk_{}'.format(user_id)
     try :
-      self.r.set(user_id , self.keys[index +1])
-      return self.keys[index+1]
+      self.r.set(redis_key , index +1)
+      return self.questions[index+1][0]
         
     except IndexError:
-      return self.keys[0] 
+      self.r.set(redis_key , 0)
+      return self.questions[0][0] 
 
 
   def answer_bot(self, event): 
     text = event.text
     index = self.get_question_index(event.user_id)
-
+  
     if text == 'Новый вопрос':
       return self.new_question(event.user_id , index)
 
     elif text ==  'Сдаться' :
       if index == -1:
         return 'Рано сдаётесь , вопрос ещё не задан !'
-      return self.questions[self.keys[index]]
+      return self.questions[index][1]
 
     elif text == 'Мой счёт':
       return 'Статистика не ведётся' 
@@ -60,7 +64,7 @@ class MyVkBot():
     else:
       if index > -1:
         text = text.lower()  
-        right_answer = self.questions[self.keys[index]].lower()
+        right_answer = self.questions[index][1].lower()
         
         if '(' in right_answer:
           right_answer = right_answer[0:right_answer.find('(')]
@@ -82,6 +86,7 @@ class MyVkBot():
                     message = self.answer_bot(event),
                     random_id = random.randint(1,1000),
                     keyboard = keyboard)
+
     if event.text == 'Сдаться':
       event.text = 'Новый вопрос'
       api.messages.send(user_id  = event.user_id,
